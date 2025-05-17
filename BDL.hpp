89b99c0a9674c9bb78ef5a25977d234b9c40e9a8
The-bit-wielder <138500792+The-bit-wielder@users.noreply.github.com>
2025-05-17 12:59:47 +0300

    Update BDL.hpp

--- BDL.hpp
+++ BDL.hpp
@@ -1,4 +1,33 @@
+#ifndef BDL
+
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <cstdlib>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::cerr;
+
+namespace BDL {
+//Variables
+	bool initialized = false;
+	string globalString;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="str"></param>
+	void globalStringFunc(std::string &str) {
+		if (!initialized) {
+			globalString = "Beginning of log\n";
+			initialized = true;
+			globalString::append(str);
+			globalString::append("\n");
+		}
+		globalString::append(str);
+	}
+} // namespace BDL
+#endif //BDL
